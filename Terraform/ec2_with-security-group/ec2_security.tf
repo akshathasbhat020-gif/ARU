c1e082bcf8b4a27e1c729a6ec278e9e63c800765
Akshatha <akshathasbhat020@gmail.com>
2026-03-20 19:55:27 +0530

    edited

--- Terraform/ec2_with-security-group/ec2_security.tf
+++ Terraform/ec2_with-security-group/ec2_security.tf
@@ -2,12 +2,6 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-resource "aws_instance" "arun" {
-    ami = "ami-0ecfdfd1c8ae01aec"
-    instance_type = "t3.micro"
-    security_groups = [aws_security_group.terraform.id]
-}
-
 resource "aws_security_group" "terraform" {
     name = "Terraform"
 # Inbound rules
@@ -41,3 +35,9 @@ resource "aws_security_group" "terraform" {
             cidr_blocks = ["0.0.0.0/0"]
         }
     }
+
+resource "aws_instance" "arun" {
+    ami = "ami-0ecfdfd1c8ae01aec"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.terraform.id]
+}
