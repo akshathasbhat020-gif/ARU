7575ae89adaf75aa5bdc54c5fdd286a0531cfb71
Akshatha <akshathasbhat020@gmail.com>
2026-03-20 18:24:01 +0530

    added bucket

---
+++ Terraform/s3.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "ap-northeast-2"
+    access_key = ""
+    secret_key = ""
+    }
+resource "aws_s3_bucket" "Terraform-bucket" {
+    bucket = "terraform-bucket-2024"
+}
