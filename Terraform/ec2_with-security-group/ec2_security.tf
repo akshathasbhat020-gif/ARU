22e7a17755a032a8bedec8cc15940e4eca4662c2
Akshatha <akshathasbhat020@gmail.com>
2026-03-20 19:37:13 +0530

    security groups created

---
+++ Terraform/ec2_with-security-group/ec2_security.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+resource "aws_instance" "arun" {
+    ami = "ami-0ecfdfd1c8ae01aec"
+    instance_type = "t3.micro"
+    security_groups = []
+}
+
+resource "aws_security_group" "terraform" {
+    name = "Terraform"
+# Inbound rules
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+# Outbound rules
+    egress {
+            from_port = 0
+            to_port = 0
+            protocol = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
